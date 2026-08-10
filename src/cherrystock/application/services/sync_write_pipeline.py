db7c4a32713b0db4f65bb7b59f9e2baa8fdeb45e
haithanh8x <haithanh8x@gmail.com>
2026-08-10 17:05:38 +0700

    Fix src-layout imports in sync write pipeline

--- src/cherrystock/application/services/sync_write_pipeline.py
+++ src/cherrystock/application/services/sync_write_pipeline.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Callable
 
-from src.CrawlStock.readAmi import syncAmibroker_EOD, upsert_lstTicker, upsert_stock_fa
-from src.CrawlStock.readYahooFinance import syncYahooFinance_EOD
-from src.Ults.DuckLib import executeDuckSQL
-from src.Ults.lstPara import DUCKDB_SQL_PATH
-from src.calcEngine import calc_fv_Trend
-from src.calcEngine.calcIndexes import calculate_VNINDEX_NOT_VIN
+from CrawlStock.readAmi import syncAmibroker_EOD, upsert_lstTicker, upsert_stock_fa
+from CrawlStock.readYahooFinance import syncYahooFinance_EOD
+from Ults.DuckLib import executeDuckSQL
+from Ults.lstPara import DUCKDB_SQL_PATH
+from calcEngine import calc_fv_Trend
+from calcEngine.calcIndexes import calculate_VNINDEX_NOT_VIN
 
 
 class SyncWritePipelineService:
@@ -48,6 +48,10 @@ class SyncWritePipelineService:
         self._sync_yahoo_eod(from_last_day=days_diff, connection=connection)
         self._upsert_fa(amibroker=amibroker, connection=connection)
         self._upsert_tickers(connection=connection, repository=ticker_repository)
-        self._execute_sql(con=connection,sql_file_path=str(self._sql_dir / "updateHoliday.sql"))
+        self._execute_sql(con=connection, sql_file_path=str(self._sql_dir / "updateHoliday.sql"))
         self._calc_index(connection=connection, repository=index_repository)
-        self._calc_trend(from_last_day=days_diff,connection=connection,repository=trend_repository,)
+        self._calc_trend(
+            from_last_day=days_diff,
+            connection=connection,
+            repository=trend_repository,
+        )
