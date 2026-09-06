9d31ed5ce8ced3cb5823218a22a69a50f7c2461f
haithanh8x <haithanh8x@gmail.com>
2026-09-06 15:49:20 +0700

    auto-sync: 2026-09-06 15:49:19

--- src/DuckDB/sql/transforms/Test.sql
+++ src/DuckDB/sql/transforms/Test.sql
@@ -1,3 +1,7 @@
 
 
 select * from "CherryMon"."main"."vw_Ticker_OHLC_D" where ticker='MWG' order by date desc;
+
+select * from "CherryMon"."main"."vw_raw_stock_eod" where ticker='PNJ'  order by date desc;
+
+select * from "CherryMon"."main"."vw_raw_stock_eod" where date>='2026-06-01';
