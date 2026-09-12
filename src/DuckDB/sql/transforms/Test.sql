880ace62b729a63195f4317c1766da52034242cd
haithanh8x <haithanh8x@gmail.com>
2026-09-12 18:06:26 +0700

    auto-sync: 2026-09-12 18:06:26

--- src/DuckDB/sql/transforms/Test.sql
+++ src/DuckDB/sql/transforms/Test.sql
@@ -1,12 +1,26 @@
+select * from "CherryMon"."main"."vw_Ticker_indicators";
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' and ticker='MWG' order by date desc ;
 
 
-select * from "CherryMon"."main"."vw_Ticker_OHLC_D" where ticker='MWG' order by date desc;
 
-select * from "CherryMon"."main"."vw_raw_stock_eod" where ticker='PNJ'  order by date desc;
 
-select * from "CherryMon"."main"."vw_raw_stock_eod" where date>='2026-06-01';
 
-select * from "CherryMon"."main"."dim_indicator";
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
+and MarketState = 'ACCUMULATION'
+and AccumulationScore >= 65
+and AccumulationMemoryScore >= 60
+and RelativeStrengthScore >= 50
+and DistributionScore < 50
+and ConfidenceScore >= 70;
 
-select * from "CherryMon"."main"."dim_indicator_component";
-select * from "CherryMon"."main"."dim_indicator_config";
+
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
+and SupplyLockScore >= 70
+and AccumulationMemoryScore >= 60;
+
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
+and FreshFlowScore >= 70
+and RelativeLiquidityScore >= 70;
+
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
+and DistributionScore >= 70
