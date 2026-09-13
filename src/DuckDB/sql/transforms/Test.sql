b2c48f3d7e9344a0126ae54b185c04125985fbc6
haithanh8x <haithanh8x@gmail.com>
2026-09-13 17:16:23 +0700

    auto-sync: 2026-09-13 17:16:23

--- src/DuckDB/sql/transforms/Test.sql
+++ src/DuckDB/sql/transforms/Test.sql
@@ -1,34 +1,8 @@
 
 select * from "CherryMon"."main"."vw_Ticker_indicators";
-select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' and ticker='MWG' order by date desc ;
-
-
-
-
-
-select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
-and MarketState = 'ACCUMULATION'
-and AccumulationScore >= 65
-and AccumulationMemoryScore >= 60
-and RelativeStrengthScore >= 50
-and DistributionScore < 50
-and ConfidenceScore >= 70;
-
-
-select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
-and SupplyLockScore >= 70
-and AccumulationMemoryScore >= 60;
-
-select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' 
-and FreshFlowScore >= 70
-and RelativeLiquidityScore >= 70;
 
 select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' ;
 
+select * from "CherryMon"."main"."vw_Ticker";
 
-select * from "CherryMon"."main"."dim_smart_money_config";
-
-
-& C:\Program1\Python\Python313\python.exe c:/Github/CherryStock/src/webapp/NiceGUI_chart.py;
-
-& scripts\git_auto_sync.ps1;
+select * from CherryMon.main.raw_stock_fa;
