6692032533a0f9a4602f8a5e4344deb15a2272d4
haithanh8x <haithanh8x@gmail.com>
2026-09-15 22:14:08 +0700

    feat(price-movement): expose repository

--- src/cherrystock/infrastructure/database/repositories/__init__.py
+++ src/cherrystock/infrastructure/database/repositories/__init__.py
@@ -1,8 +1,17 @@
 from cherrystock.infrastructure.database.repositories.index_repository import IndexRepository
 from cherrystock.infrastructure.database.repositories.indicator_repository import IndicatorRepository
+from cherrystock.infrastructure.database.repositories.price_movement_repository import PriceMovementRepository
 from cherrystock.infrastructure.database.repositories.rs_evaluation_repository import RSEvaluationRepository
 from cherrystock.infrastructure.database.repositories.smart_money_repository import SmartMoneyRepository
 from cherrystock.infrastructure.database.repositories.ticker_repository import TickerRepository
 from cherrystock.infrastructure.database.repositories.trend_repository import TrendRepository
 
-__all__ = ["IndexRepository", "IndicatorRepository", "RSEvaluationRepository", "SmartMoneyRepository", "TickerRepository", "TrendRepository"]
+__all__ = [
+    "IndexRepository",
+    "IndicatorRepository",
+    "PriceMovementRepository",
+    "RSEvaluationRepository",
+    "SmartMoneyRepository",
+    "TickerRepository",
+    "TrendRepository",
+]
