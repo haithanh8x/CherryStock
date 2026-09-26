f5ef0c9349818cf74a349b9be7b309d1314cfff9
haithanh8x <haithanh8x@gmail.com>
2026-09-26 17:55:45 +0700

    auto-sync: 2026-09-26 17:55:45

--- src/DuckDB/sql/transforms/Test.sql
+++ src/DuckDB/sql/transforms/Test.sql
@@ -1,15 +1,10 @@
 
-select * from "CherryMon"."main"."vw_Ticker_indicators";
-
-select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where date='2026-09-11' ;
-
-select * from "CherryMon"."main"."vw_Ticker";
-
-select * from "CherryMon"."main"."raw_stock_fa";
-
-
 select * from "CherryMon"."main"."vw_Ticker_ZigZag_Swings";
 
 select * from "CherryMon"."main"."vw_Ticker_Price_Movement_Swings";
 
-select * from "CherryMon"."main"."vw_Ticker_Movement_Context";
+select * from "CherryMon"."main"."vw_Ticker_Movement_Context" where "Ticker" = 'MWG';
+
+select * from "CherryMon"."main"."vw_Ticker_Movement_Profile" where "Ticker" = 'MWG';
+
+select * from "CherryMon"."main"."vw_Ticker_SmartMoney" where "Ticker" = 'MWG';
